6a454b67ab2e196dcb5bcd45e86636a1a2516bfd
shafriki <shafriki.cse@gmail.com>
2025-03-08 16:38:49 +0600

    added cors origin op

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 const corsOptions = {
-  origin: ['http://localhost:5173', 'https://robendevs-handson.web.app'],
+  origin: ['http://localhost:5173', 'https://robendevs-handson.web.app','https://robendevs-handson.firebaseapp.com'],
   credentials: true,
   optionSuccessStatus: 200,
 };
